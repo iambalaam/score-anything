a30c4d6937ba9ff3831cea9a2c39bebcd07002bd
Steve Beecheno <iamhacktinium@gmail.com>
2023-02-04 11:35:48 +0000

    create Dial component. import into App

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
-import * as React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import Dial from "./components/Dail";
+import "./index.css";
 
 function App() {
-    return <h1>Score Anything</h1>;
+	return (
+		<>
+			<h1>Score Anything</h1>
+			<Dial />
+		</>
+	);
 }
 
-const HTML_ROOT = document.createElement('div');
-HTML_ROOT.id = 'root';
+const HTML_ROOT = document.createElement("div");
+HTML_ROOT.id = "root";
 document.body.appendChild(HTML_ROOT);
 
-const REACT_ROOT = createRoot(document.getElementById('root')!);
+const REACT_ROOT = createRoot(document.getElementById("root")!);
 REACT_ROOT.render(<App />);
