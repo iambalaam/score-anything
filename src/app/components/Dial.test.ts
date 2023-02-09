15286f543e919837d823174f0e3ff5cd6bf7af33
Guy Balaam <guy.balaam@unity3d.com>
2023-02-09 18:36:26 +0000

    Updating tests

--- src/app/components/Dial.test.ts
+++ src/app/components/Dial.test.ts
@@ -1,4 +1,7 @@
+import { HSL } from '../util/color';
 import { clamp, conicGradient } from './Dial';
+
+const BLUE = new HSL(208, 64, 57);
 
 describe('clamp()', () => {
     it('returns an unclamped value', () => {
@@ -23,13 +26,13 @@ describe('clamp()', () => {
 
 describe('conicGradient()', () => {
     it('is a valid css class', () => {
-        const gradient = conicGradient(90, 'indianred', 'transparent');
+        const gradient = conicGradient(90, BLUE, 'transparent');
         expect(gradient.startsWith('conic-gradient(')).toBeTruthy();
         expect(gradient.endsWith(')')).toBeTruthy();
     })
 
     it('handles the given color', () => {
-        expect(conicGradient(90, 'indianred', 'transparent'))
-            .toBe('conic-gradient(transparent 0deg, indianred 0deg, indianred 90deg, transparent 90deg)');
+        expect(conicGradient(90, BLUE, 'transparent'))
+            .toBe('conic-gradient(transparent 0deg, hsl(214.80239476332432deg, 64%, 43.395210473351376%) 0deg, hsl(208deg, 64%, 57%) 90deg, transparent 90deg)');
     })
 });
