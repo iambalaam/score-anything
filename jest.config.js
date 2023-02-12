38004e5c77b31505ff15fc72030e703d6ccfb1b0
Guy Balaam <guy.balaam@unity3d.com>
2023-02-12 19:03:23 +0000

    Fixing test warnings

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
-    '\\.tsx?$': ['ts-jest', { tsConfig: 'src/app/tsconfig.json' }],
+    '\\.tsx?$': ['ts-jest', { tsconfig: 'src/app/tsconfig.json' }],
     '\\.css': '<rootDir>/src/app/__mocks__/fileMock.js'
   }
 };
