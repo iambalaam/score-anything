85c3b096b8e0c7e0d4488ec391abf750826651dc
iambalaam <iambalaam@users.noreply.github.com>
2023-05-15 10:40:54 +0000

    Deploying to gh-pages from @ iambalaam/score-anything@023c3d6fe0f7cf7976ae3a1392ade667b2fe04f9 🚀

--- sw.js
+++ sw.js
@@ -1,15 +1,17 @@
 "use strict";
+const SW_FILE = '/sw.js';
+const root = self.location.pathname.replace(SW_FILE, '');
 const cacheFiles = [
-    '/',
-    '/index.html',
-    '/main.bundle.js',
-    '/main.css',
-    '/manifest.json',
-    '/static/icon-500.png',
+    '',
+    'index.html',
+    'main.bundle.js',
+    'main.css',
+    'manifest.json',
+    'static/icon-500.png',
 ];
 self.addEventListener('install', (event) => {
     event.waitUntil(caches.open('v1').then((cache) => {
-        return cache.addAll(cacheFiles);
+        return cache.addAll(cacheFiles.map((file) => `${root}/${file}`));
     }));
 });
 const putInCache = async (request, response) => {
