2876cd4108cbc12d7b49ebd39d81bfb96bc2ca72
Guy Balaam <guy.balaam@unity3d.com>
2023-05-15 10:55:12 +0100

    Updating to non-root serviceworker for Github pages

--- src/app/html.js
+++ src/app/html.js
@@ -5,6 +5,7 @@ function htmlTemplate({ htmlWebpackPlugin }) {
         <title>Score Anything</title>
         <link rel="manifest" href="manifest.json" />
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
+        <link rel="icon" type="image/x-icon" href="/static/icon-500.png">
         ${htmlWebpackPlugin.tags.headTags}
     </head>
     <body>
