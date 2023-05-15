023c3d6fe0f7cf7976ae3a1392ade667b2fe04f9
Guy Balaam <guy.balaam@unity3d.com>
2023-05-15 11:30:20 +0100

    Adding serviceworker fetch intercept

--- src/worker/sw.ts
+++ src/worker/sw.ts
@@ -1,19 +1,42 @@
+interface Window extends ServiceWorkerGlobalScope { }
+interface WindowEventMap extends ServiceWorkerGlobalScopeEventMap { }
+
+const SW_FILE = '/sw.js';
+const root = self.location.pathname.replace(SW_FILE, '');
 const cacheFiles = [
-    '/main.bundle.js',
-    '/main.css',
-    '/manifest.json',
-    '/static/icon-500.png',
+    '',
+    'index.html',
+    'main.bundle.js',
+    'main.css',
+    'manifest.json',
+    'static/icon-500.png',
 ];
 
-const addResourcesToCache = async (resources: string[]) => {
-    const cache = await caches.open("v1");
-    await cache.addAll(resources);
-};
-
 self.addEventListener('install', (event) => {
-    console.log('[[[ installed ]]]');
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
+            return cache.addAll(
+                cacheFiles.map((file) => `${root}/${file}`)
+            );
+        })
+    );
 });
 
-self.addEventListener('fetch', (_event) => {
-    console.log('[[[ fetching ]]]');
+const putInCache = async (request: Request, response: Response) => {
+    const cache = await caches.open("v1");
+    await cache.put(request, response);
+};
+
+const cacheFirst = async (request: Request) => {
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+        return responseFromCache;
+    }
+    const responseFromNetwork = await fetch(request);
+    putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+};
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(cacheFirst(event.request));
 });
