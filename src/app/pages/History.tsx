0f7e038fd781a274440dff143f94599298b3322a
Guy Balaam <guy.balaam@gmail.com>
2023-08-18 18:21:13 +0100

    Adding history diffs

--- src/app/pages/History.tsx
+++ src/app/pages/History.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { SessionProps } from './Session';
 import './History.css';
 import { HSL2String } from '../util/color';
+import { toAbsFloorSignedIntString } from '../util/math';
 
 export function History({ data, setPage }: SessionProps) {
     const gridTemplateColumns = Array(data.history[0].length).fill('1fr').join(' ');
@@ -15,11 +16,20 @@ export function History({ data, setPage }: SessionProps) {
             </div>
             <div id="grid" style={{ gridTemplateColumns }}>
                 {
-                    data.history.map((entry) =>
-                        entry.map((score, i) =>
-                            <span className='score' style={{ color: HSL2String(data.counters[i].color) }} key={i}>{score}</span>
+                    [...data.history]
+                        .map((entry, i) =>
+                            entry.map((score, player) => {
+                                if (i !== 0) {
+                                    const diff = toAbsFloorSignedIntString(data.history[i][player] - data.history[i - 1][player]);
+                                    if (diff !== '0') {
+                                        return <span className='score' style={{ color: HSL2String(data.counters[player].color) }} key={player} data-diff={diff}>{score}</span>
+                                    }
+                                }
+                                return <span className='score' style={{ color: HSL2String(data.counters[player].color) }} key={player}>{score}</span>
+                            })
                         )
-                    )}
+                        .reverse()
+                }
             </div>
         </main>)
 }
