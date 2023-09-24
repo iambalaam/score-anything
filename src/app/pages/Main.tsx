e1d703e2384b98774ce3b7c9c54d2452129fa563
Guy Balaam <hi@iambalaam.com>
2023-09-24 09:24:48 +0100

    MUI Buttons on main

--- src/app/pages/Main.tsx
+++ src/app/pages/Main.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
 import './Main.css';
+import Button from '@mui/material/Button';
+import CreateNewFolderIcon from '@mui/icons-material/CreateNewFolder';
+import FolderOpenIcon from '@mui/icons-material/FolderOpen';
 import { Page } from '../app';
 
 export interface MainProps {
@@ -9,8 +12,20 @@ export interface MainProps {
 export function Main({ setPage }: MainProps) {
     return (
         <main id="main">
-            <button onClick={() => setPage('player-setup')}>New Game</button>
-            <button onClick={() => setPage('session-select')}>Previous Games</button>
+            <Button
+                startIcon={<CreateNewFolderIcon />}
+                variant="contained"
+                onClick={() => setPage('player-setup')}
+            >
+                New Game
+            </Button>
+            <Button
+                startIcon={<FolderOpenIcon />}
+                variant="contained"
+                onClick={() => setPage('session-select')}
+            >
+                Previous Games
+            </Button>
         </main>
     );
 }
